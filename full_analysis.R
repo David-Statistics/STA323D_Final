25c874ad0153aa2cd388c35ab74cb9cdef394059
David Clancy <david.statistics@gmail.com>
2016-05-07 05:03:17 -0400

    Adding comments

--- full_analysis.R
+++ full_analysis.R
@@ -2,9 +2,11 @@ library(parallel)
 library(tictoc)
 library(tm)
 library(tools)
+## Get data files
 files = list.files("./Rdata")
 files = files[grep("Philly", files)]
 
+## Pull positive and negative words
 neg_words = unlist(read.csv("negative_words.txt", stringsAsFactors = FALSE))
 pos_words = unlist(read.csv("postive_words.txt", stringsAsFactors = FALSE))
 pos_words = pos_words[-grep("trump", pos_words)]
@@ -13,10 +15,14 @@ names(named_pos) = pos_words
 named_neg = rep(-1, length(neg_words))
 names(named_neg) = neg_words
 
+## Initialize full data frame and loop over the files
 full.df = NULL
 for (file in files) {
   load(paste0("./Rdata/",file))
+  ## Grab and clean the tweets
   new.df = mclapply(tweets$text, function(s) {
+    ## Remove punctuation, change problematic characters, split on whitespace,
+    ## remove links, and score the tweet
     words = gsub("[[:punct:]]", "", s)
     words = iconv(words, to = "utf-8", sub = "")
     words = strsplit(words, "\\s")[[1]]
@@ -28,20 +34,21 @@ for (file in files) {
     c(paste(words, collapse = " "), score)
   }, mc.cores = 24)
   
-  
+  ## Combine the new data with the old
   new.df = as.data.frame(do.call(rbind, new.df), stringsAsFactors = FALSE)
   new.df[,2] = as.numeric(new.df[,2])
   names(new.df) = c("text", "score")
   new.df = cbind(new.df, tweets[,2:3])
   full.df = rbind(full.df, new.df)
 }
+## Remove duplicates (retweets) and convert created_at to an actual time vector
 full.df = full.df[!duplicated(full.df$text), ]
 full.df$created_at = strptime(sapply(strsplit(full.df$created_at, "\\s"), function(x) {
   paste(x[1:4], collapse = " ")
   }), "%a %b %d %H:%M:%S")
 full.df$secs = as.numeric(full.df$created_at)
 
-
+## Classify tweets as talking about a candidate by using common words/hashtags
 candidates = list(c("trump", "donald", "makeamericagreatagain"), 
                   c("kasich"), 
                   c("cruz", "tedcruz"),
@@ -53,9 +60,11 @@ mentions = mclapply(full.df$text, function(s) {
   })
 }, mc.cores = 24)
 mentions = do.call(rbind, mentions)
+## Append the mentions onto the original full data frame
 candidate_names = toTitleCase(sapply(candidates, function(c) c[1]))
 colnames(mentions) = candidate_names
 full.df = cbind(full.df, mentions)
+## Add a column for either the single candidate mentioned or multiple candidates
 cands = unlist(mclapply(1:nrow(full.df), function(i) {
   if(sum(full.df[i, 6:10] == TRUE)>1) {return("Multiple")}
   if(sum(full.df[i, 6:10] == TRUE)==0) {return("None")}
@@ -64,21 +73,24 @@ cands = unlist(mclapply(1:nrow(full.df), function(i) {
   }
 }, mc.cores = 24))
 full.df$Candidate = factor(cands)
+## Remove tweets that don't mention any
 full.df = full.df[full.df$Candidate != "None", ]
 full.df$Candidate = droplevels(full.df$Candidate)
 save(full.df, file = "full_data.Rdata")
 
-
+## Construct a vector corpus based on the candidates
 vc = sapply(candidate_names, function(s) {
   inds = which(full.df[[s]])
   text = iconv(enc2utf8(paste(full.df$text[inds], collapse = " ")), to = 'UTF-8')
   iconv(text, "UTF-8", "ASCII", sub="")
 })
 VC = VCorpus(VectorSource(vc))
-inspect(VC)
+
+## Create tfidf scores for the candidates and words
 tdm = TermDocumentMatrix(VC)
 tfidf = weightTfIdf(tdm)
 
+## convert the tfidf object to a matrix for ease of use
 tfidf.m = as.matrix(tfidf)
 save(tfidf, file = "tfidf.Rdata")
 save(tfidf.m, file = "tfidf.m.Rdata")
--- listenPhilly.R
+++ listenPhilly.R
@@ -5,12 +5,11 @@ library(streamR)
 library(lubridate)
 time_s = paste0(hour(Sys.time()), ".", minute(Sys.time()))
 
-
 filterStream(file.name = paste0("/home/vis/djc37/STA323D_Final/data/tweetsPhilly", time_s, ".json"), # Save tweets in a json file
              track = c("trump", "kasich", "cruz", "hillary", "bernie", "clinton", "primary",
                        "fightingforus", "feelthebern", "makeamericagreatagain",
-                       "nevertrump", "drumpf", "Elections2016"), 
-             language = "en",
-             location = c(-75.338745, 39.857046, -74.84436, 40.049694), #-124, 25, -64, 49 
-             timeout = 600, # Keep connection alive for 60 seconds 40.11799 | Longitude: -75.39505
+                       "nevertrump", "drumpf", "Elections2016"), # Terms to track
+             language = "en", # Only english tweets
+             location = c(-75.338745, 39.857046, -74.84436, 40.049694), # Philly
+             timeout = 600, # Keep connection alive for 60 seconds 
              oauth = my_oauth) # Use my_oauth file as the OAuth credentials
